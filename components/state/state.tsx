1aefeda2f36aed9b74cdbcec22e08c6372b6c81e
bisratawoke <bisratawokegithub@gmail.com>
2021-06-29 02:33:09 +0000

    changed

--- components/state/state.tsx
+++ components/state/state.tsx
@@ -3,7 +3,7 @@ import {createContext} from 'react';
 //init state
 export const state = {
 
-    apiUrl:"http://account/api/account"
+    apiUrl:"http://swiftbase.com/api/account"
 
 }
 //context creation
