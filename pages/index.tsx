37535f640ec8b95f41c986ab1ddc4ff60e1df17f
bisratawoke <bisratawokegithub>
2021-07-11 18:46:57 +0300

    minor changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -183,19 +183,19 @@ function index() {
                 </div>
 
                 
-                <div className="h-footer-section bg-gray-700">
-                  <div className="h-20 bg-gray-100"></div>
+                <div className="h-footer-section bg-gray-700 pb-5">
+                  <div className="h-10 bg-gray-100"></div>
                   <div className="h-full grid grid-cols-12 bg-gray-100">
 
                       <div className="col-start-3 col-end-11  grid grid-cols-5">
 
-                          <div className="flex flex-col items-center">
+                          <div className="flex flex-col items-center gap-2">
 
                                 <span className="text-xl font-normal font-sans text-gray-500">Services</span>
                                 <span className="text-md font-normal font-sans text-gray-500 cursor-pointer hover:text-red-500">Frontend service</span>
                                 <span className="text-md font-normal font-sans text-gray-500 cursor-pointer hover:text-red-500">Backend service</span>
                           </div>
-                          <div className=" flex flex-col items-center">
+                          <div className=" flex flex-col items-center gap-2">
                                 
                                 <span className="text-xl font-normal font-sans text-gray-500">Use cases</span>
 
@@ -204,14 +204,13 @@ function index() {
                                 <span className="text-md font-normal font-sans text-gray-500 cursor-pointer hover:text-red-500">full project use case</span>
                                 
                           </div>
-                          <div className="flex flex-col items-center">
+                          <div className="flex flex-col items-center gap-1">
                                 <span className="text-xl font-normal font-sans text-gray-500">Resources</span>
                                 <span className="text-md font-normal font-sans text-gray-500 cursor-pointer hover:text-red-500">Documentation</span>
                                 <span className="text-md font-normal font-sans text-gray-500 cursor-pointer hover:text-red-500">User guides</span>
-                                <span className="text-md font-normal font-sans text-gray-500 cursor-pointer hover:text-red-500">Get started guide</span>
                                 <span className="text-md font-normal font-sans text-gray-500 cursor-pointer hover:text-red-500">pricing info</span>
                           </div>
-                          <div className="flex flex-col items-center">
+                          <div className="flex flex-col items-center gap-2">
                                 <span className="text-xl font-normal font-sans text-gray-500">Support</span>
                                 <span className="text-md font-normal font-sans text-gray-500 cursor-pointer hover:text-red-500">About us</span>
                                 <span className="text-md font-normal font-sans text-gray-500 cursor-pointer hover:text-red-500">Contact us</span>
